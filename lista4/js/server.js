71bf77f05c93957f9e6b66d5efbe28f79ae587f6
AHerda <adrianherda@gmail.com>
2023-05-24 01:26:53 +0200

    proby

---
+++ lista4/js/server.js
@@ -0,0 +1,18 @@
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
+var server = http.createServer( function (request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+    fs.readFile(pathname.substr(1), function (err, data) {
+        if (err) {
+            console.log(err);
+            response.writeHead(404, {"Content-Type": "text/html"});
+        } else {
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.write(data.toString());
+        }
+        response.end();
+    });
+}).listen(3000);
+console.log("Serwer nasłuchuje na http://127.0.0.1:3000/");
