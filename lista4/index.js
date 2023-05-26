fc7f0f1765619619ad5c2c03b8231f4b9241fec6
AHerda <adrianherda@gmail.com>
2023-05-26 16:22:44 +0200

    zadanie 10%

--- lista4/index.js
+++ lista4/index.js
@@ -1,2 +1,8 @@
-var express = reequire("express");
-var mongoose = require("mongoose");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+mongoose.connect('mongodb://127.0.0.1:27017/notes')
+.then(() => {console.log("Connected to Database!");})
+.catch(() => {console.error("Error connecting to Database");});
