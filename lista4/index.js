10328e74e9b0ac66902a5837c9dc8aa52e316de5
AHerda <adrianherda@gmail.com>
2023-05-28 02:16:40 +0200

    na szybko

--- lista4/index.js
+++ lista4/index.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 const app = express();
-mongoose.connect('mongodb://127.0.0.1:27017/notes')
-.then(() => {console.log("Connected to Database!");})
-.catch(() => {console.error("Error connecting to Database");});
+
+mongoose.connect("mongodb://127.0.0.1:27017/notes", { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {console.log("Connected to Database!");})
+    .catch(() => {console.error("Error connecting to Database");});
+
