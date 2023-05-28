7a6dadd44a4bc970f9d7928827710b8fff81dfa2
AHerda <adrianherda@gmail.com>
2023-05-28 02:18:30 +0200

    porządki

--- lista4/index.js
+++ lista4/index.js
@@ -6,3 +6,11 @@ const app = express();
 mongoose.connect('mongodb://127.0.0.1:27017/notes')
 .then(() => {console.log("Connected to Database!");})
 .catch(() => {console.error("Error connecting to Database");});
+
+app.get("/", )
+
+const server = app.listen(3000, () => {
+    var address = server.address().address;
+    var port = server.address().port;
+    console.log("Server listening to https://%s:%s/", address, port);
+})
